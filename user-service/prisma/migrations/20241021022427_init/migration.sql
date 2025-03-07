ccf29599f59c15285e85bd797e6b46af18a74107
Sam <s4muelrp@gmail.com>
2025-03-07 20:13:43 -0300

    update Dockerfile and docker-compose for user service, add healthcheck, and adjust environment settings

--- user-service/prisma/migrations/20241021022427_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "questoe_conteudos" DROP CONSTRAINT "questoe_conteudos_questaoId_fkey";
-
--- AddForeignKey
-ALTER TABLE "questoe_conteudos" ADD CONSTRAINT "questoe_conteudos_questaoId_fkey" FOREIGN KEY ("questaoId") REFERENCES "questoes"("id") ON DELETE CASCADE ON UPDATE CASCADE;
