9a7fc64a789394a6f45a0c98d23dcce54be3fb6e
Sam <s4muelrp@gmail.com>
2025-03-07 20:42:01 -0300

    fix: update PrismaClient import to correct package path

--- user-service/src/config/RegistrarDependencias.config.ts
+++ user-service/src/config/RegistrarDependencias.config.ts
@@ -1,5 +1,5 @@
 
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
 import { gerenciadorDeDependencias } from "./GerenciadorDeDependencias.service";
 
 const prismaClient = new PrismaClient();
