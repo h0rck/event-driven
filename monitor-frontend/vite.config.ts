a591c4fcdb670066017edc0ac2da50163d201527
Sam <s4muelrp@gmail.com>
2025-03-08 17:54:45 -0300

    feat: add Dockerfile and Vite configuration for monitor frontend application

--- monitor-frontend/vite.config.ts
+++ monitor-frontend/vite.config.ts
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
+    server: {
+        port: 5173,
+        strictPort: true // Isso fará com que o Vite falhe se a porta 5173 não estiver disponível, ao invés de tentar outra porta
+    }
 })
