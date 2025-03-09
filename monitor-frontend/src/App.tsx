64e897dbe3bda7fe2f9fcb38c1a97289e1eab034
Sam <s4muelrp@gmail.com>
2025-03-09 13:26:05 -0300

    refactor: remove Dashboard and Notifications components; update routing to use Monitor as the main view

--- monitor-frontend/src/App.tsx
+++ monitor-frontend/src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
 import Monitor from './components/Monitor';
-import Notifications from './components/Notifications';
 
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/monitor" element={<Monitor />} />
-                <Route path="/notifications" element={<Notifications />} />
+                <Route path="/" element={<Monitor />} />
             </Routes>
         </Router>
     );
