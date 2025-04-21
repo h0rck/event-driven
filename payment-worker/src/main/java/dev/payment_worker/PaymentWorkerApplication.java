21f6532578beca4e058926fd73a68c2e2773c225
Sam <s4muelrp@gmail.com>
2025-04-21 17:03:18 -0300

    feat: add initial payment worker service setup with Maven and application properties

---
+++ payment-worker/src/main/java/dev/payment_worker/PaymentWorkerApplication.java
@@ -0,0 +1,13 @@
+package dev.payment_worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentWorkerApplication.class, args);
+	}
+
+}
---
+++ payment-worker/src/test/java/dev/payment_worker/PaymentWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.payment_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
