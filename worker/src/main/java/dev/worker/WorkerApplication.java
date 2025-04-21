dc0ba7ad91079fdfc8f34950bdf9749b614b9544
Sam <s4muelrp@gmail.com>
2025-04-21 16:53:00 -0300

    feat: initialize worker service with Maven configuration and application properties

--- worker/src/main/java/dev/worker/WorkerApplication.java
+++ worker/src/main/java/dev/worker/WorkerApplication.java
@@ -1,5 +1,8 @@
 package dev.worker;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class WorkerApplication {
 
