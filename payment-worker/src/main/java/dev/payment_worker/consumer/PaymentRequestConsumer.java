d8583d2c0b4463cee91f84526ced179dfec7bf6e
Sam <s4muelrp@gmail.com>
2025-04-21 18:20:44 -0300

    feat: update payment request consumer to use generic message type and fix application configuration

--- payment-worker/src/main/java/dev/payment_worker/consumer/PaymentRequestConsumer.java
+++ payment-worker/src/main/java/dev/payment_worker/consumer/PaymentRequestConsumer.java
@@ -1,16 +1,16 @@
 package dev.payment_worker.consumer;
 
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PaymentRequestConsumer {
 
-    @RabbitListener(queues = "${payment-request-queue}")
-    public void consumePaymentRequest(@Payload Message message) {
-        System.out.println("Processing payment request: " + message);
+    @RabbitListener(queues = { "payment-request-queue" })
+    public void consumePaymentRequest(@Payload Message<String> message) {
+        System.out.println("Processing payment request: " + message.getPayload());
     }
-
 }
